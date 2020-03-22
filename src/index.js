0a3f350ba9308d95b13f2361dfed344b7a79ce05
Leonid Petrov <lp.msarchitect@gmail.com>
2020-03-22 15:16:45 +0300

    feat: Add solition

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array = null) {
+    if (array === null || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((minItem, currentItem) => {
+        return minItem > currentItem ? currentItem : minItem;
+    });
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array = null) {
+    if (array === null || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((maxItem, currentItem) => {
+        return maxItem < currentItem ? currentItem : maxItem;
+    });
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array = null) {
+    if (array === null || array.length === 0) {
+        return 0;
+    }
+    const sum = array.reduce((sum, currentItem) => {
+        return sum + currentItem;
+    }, 0);
+    return sum / array.length;
+};
